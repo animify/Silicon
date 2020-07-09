cf26f9f10a8315dc67972f281120ac7788c2b756
Stefan Mansson <stefan.aotik@gmail.com>
2020-07-09 16:18:06 +0200

    docs: restructure example doc

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Silicon, Text } from '../../src';
+import { Button, Silicon, Text, Flex } from '../../src';
 import Nav from './Nav';
 import Image from '../../src/components/Image';
 
@@ -7,115 +7,124 @@ export default function App() {
     return (
         <Silicon>
             <Nav />
-            <Text size="h3" weight="bold">
-                Font Sizes
-            </Text>
 
-            <div style={{ margin: 10 }}>
-                <Text size="h1">Heading 1</Text>
-            </div>
-            <div style={{ margin: 10 }}>
-                <Text size="h2">Heading 2</Text>
-            </div>
-            <div style={{ margin: 10 }}>
-                <Text size="h3">Heading 3</Text>
-            </div>
-            <div style={{ margin: 10 }}>
-                <Text size="h4">Heading 4</Text>
-            </div>
-            <div style={{ margin: 10 }}>
-                <Text size="h5">Heading 5</Text>
-            </div>
-            <div style={{ margin: 10 }}>
-                <Text size="h6">Heading 6</Text>
-            </div>
-            <div style={{ margin: 10 }}>
-                <Text size="p">Paragraph</Text>
-            </div>
-            <div style={{ margin: 10 }}>
-                <Text size="small">Small</Text>
-            </div>
+            <Flex as="section" direction="column" mt={48}>
+                <Text size="h3" weight="bold">
+                    Font Sizes
+                </Text>
 
-            <Text size="h3" weight="bold">
-                Font Weights
-            </Text>
+                <Flex mt={12}>
+                    <Text size="h1">Heading 1</Text>
+                </Flex>
+                <Flex mt={12}>
+                    <Text size="h2">Heading 2</Text>
+                </Flex>
+                <Flex mt={12}>
+                    <Text size="h3">Heading 3</Text>
+                </Flex>
+                <Flex mt={12}>
+                    <Text size="h4">Heading 4</Text>
+                </Flex>
+                <Flex mt={12}>
+                    <Text size="h5">Heading 5</Text>
+                </Flex>
+                <Flex mt={12}>
+                    <Text size="h6">Heading 6</Text>
+                </Flex>
+                <Flex mt={12}>
+                    <Text size="p">Paragraph</Text>
+                </Flex>
+                <Flex mt={12}>
+                    <Text size="small">Small</Text>
+                </Flex>
+            </Flex>
 
-            <div style={{ margin: 10 }}>
-                <Text weight="thin">Thin</Text>
-            </div>
+            <Flex as="section" direction="column" mt={48}>
+                <Text size="h3" weight="bold">
+                    Font Weights
+                </Text>
 
-            <div style={{ margin: 10 }}>
-                <Text weight="extraLight">Extra Light</Text>
-            </div>
+                <Flex mt={12}>
+                    <Text weight="thin">Thin</Text>
+                </Flex>
 
-            <div style={{ margin: 10 }}>
-                <Text weight="light">Light</Text>
-            </div>
+                <Flex mt={12}>
+                    <Text weight="extraLight">Extra Light</Text>
+                </Flex>
 
-            <div style={{ margin: 10 }}>
-                <Text weight="regular">Regular</Text>
-            </div>
+                <Flex mt={12}>
+                    <Text weight="light">Light</Text>
+                </Flex>
 
-            <div style={{ margin: 10 }}>
-                <Text weight="medium">Medium</Text>
-            </div>
+                <Flex mt={12}>
+                    <Text weight="regular">Regular</Text>
+                </Flex>
 
-            <div style={{ margin: 10 }}>
-                <Text weight="semibold">Semibold</Text>
-            </div>
+                <Flex mt={12}>
+                    <Text weight="medium">Medium</Text>
+                </Flex>
 
-            <div style={{ margin: 10 }}>
-                <Text weight="bold">Bold</Text>
-            </div>
+                <Flex mt={12}>
+                    <Text weight="semibold">Semibold</Text>
+                </Flex>
 
-            <div style={{ margin: 10 }}>
-                <Text weight="extraBold">Extra Bold</Text>
-            </div>
+                <Flex mt={12}>
+                    <Text weight="bold">Bold</Text>
+                </Flex>
 
-            <div style={{ margin: 10 }}>
-                <Text weight="black">Black</Text>
-            </div>
+                <Flex mt={12}>
+                    <Text weight="extraBold">Extra Bold</Text>
+                </Flex>
 
-            <Text size="h3" weight="bold">
-                Image
-            </Text>
+                <Flex mt={12}>
+                    <Text weight="black">Black</Text>
+                </Flex>
+            </Flex>
 
-            <Image
-                src="https://camo.githubusercontent.com/6062000b0601afb16c465372b15edd8cb78435fd/68747470733a2f2f692e696d6775722e636f6d2f584b54556754382e706e67"
-                alt="Some image"
-                maxWidth={300}
-            />
+            <Flex as="section" direction="column" mt={48}>
+                <Text size="h3" weight="bold">
+                    Image
+                </Text>
 
-            <Text size="h3" weight="bold">
-                Buttons
-            </Text>
+                <Image
+                    src="https://camo.githubusercontent.com/6062000b0601afb16c465372b15edd8cb78435fd/68747470733a2f2f692e696d6775722e636f6d2f584b54556754382e706e67"
+                    alt="Some image"
+                    maxWidth={300}
+                />
+            </Flex>
 
-            <div style={{ margin: 10 }}>
-                <Button kind="primary">Primary Button</Button>
-            </div>
-            <div style={{ margin: 10 }}>
-                <Button kind="secondary" disabled={true}>
-                    Disabled
-                </Button>
-            </div>
-            <div style={{ margin: 10 }}>
-                <Button kind="secondary" loading={true}>
-                    Loading
-                </Button>
-            </div>
-            <div style={{ margin: 10 }}>
-                <Button
-                    kind="secondary"
-                    style={(state) => ({
-                        backgroundColor: 'red',
-                        marginTop: 100,
-                        fontWeight: state.theme.font.weight.black,
-                        nested: { ':hover': { fontWeight: state.theme.font.weight.thin } },
-                    })}
-                >
-                    Improve
-                </Button>
-            </div>
+            <Flex as="section" direction="column" mt={48}>
+                <Text size="h3" weight="bold">
+                    Buttons
+                </Text>
+
+                <Flex mt={12}>
+                    <Button kind="primary">Primary Button</Button>
+                </Flex>
+                <Flex mt={12}>
+                    <Button kind="secondary" disabled={true}>
+                        Disabled
+                    </Button>
+                </Flex>
+                <Flex mt={12}>
+                    <Button kind="secondary" loading={true}>
+                        Loading
+                    </Button>
+                </Flex>
+                <Flex mt={12}>
+                    <Button
+                        kind="secondary"
+                        style={(state) => ({
+                            backgroundColor: 'red',
+                            marginTop: 100,
+                            fontWeight: state.theme.font.weight.black,
+                            nested: { ':hover': { fontWeight: state.theme.font.weight.thin } },
+                        })}
+                    >
+                        Improve
+                    </Button>
+                </Flex>
+            </Flex>
         </Silicon>
     );
 }
