059c2753761a95e4beaf89c272b9ff19ed24a2b6
Stefan Mansson <stefan.aotik@gmail.com>
2020-07-27 22:00:06 +0200

    refactor: exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
 export * from './components';
 export * from './containers';
+export * from './theme/base';
+export * from './theme/light';
+export * from './theme/dark';
+export * from './theme/extendTheme';
