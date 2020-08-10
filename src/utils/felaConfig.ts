32eaeeee7ca5f3a1b50c449f91645431dc09578e
Stefan Mansson <stefan.aotik@gmail.com>
2020-08-10 13:31:54 +0200

    Revert "chore: remove unused imports"
    This reverts commit 73bad4d9a90d65de83cc78efdf84515c9fbcc931.

--- src/utils/felaConfig.ts
+++ src/utils/felaConfig.ts
@@ -1,8 +1,9 @@
-import { createRenderer } from 'fela';
+import { createRenderer, IStyle } from 'fela';
 import typescript from 'fela-plugin-typescript';
 import prefixer from 'fela-plugin-prefixer';
 import unit from 'fela-plugin-unit';
 import fallbackValue from 'fela-plugin-fallback-value';
+import baseTheme from '../theme/base';
 import namedKeys from 'fela-plugin-named-keys';
 
 const namedKeysPlugin = namedKeys({
